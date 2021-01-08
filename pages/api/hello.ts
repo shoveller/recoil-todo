fa3a79d38f976efa9d7f96c80e43e80c3a53cc87
fp91224 <fp91224@linecorp.com>
2021-01-08 16:20:23 +0900

    README.md 업데이트

--- pages/api/hello.ts
+++ pages/api/hello.ts
@@ -2,6 +2,12 @@
 
 import { NextApiRequest, NextApiResponse } from 'next'
 
+/**
+ * http://localhost:3000/hello를 요청했을 때 동작하는 서버 코드를 작성할 수 있습니다.
+ *
+ * @param req
+ * @param res
+ */
 const handler = (req: NextApiRequest, res: NextApiResponse) => {
   res.statusCode = 200
   res.json({ name: 'John Doe' })
