a270324930cbec404f74b4ab1f25405a172607b3
shoveller <cinos81@gmail.com>
2021-01-11 01:35:00 +0900

    모든 언어 리소스 하이드레이션을 이정도로 마무리

--- utils/i18n.ts
+++ utils/i18n.ts
@@ -2,7 +2,7 @@ import ICU, { IcuInstance } from 'i18next-icu'
 import en from 'i18next-icu/locale-data/en'
 import ko from 'i18next-icu/locale-data/ko'
 import { NextComponentType, NextPageContext } from 'next'
-import NextI18Next from 'next-i18next'
+import NextI18Next, { InitConfig } from 'next-i18next'
 import path from 'path'
 
 const use: IcuInstance[] = []
@@ -11,17 +11,19 @@ icu.addLocaleData(ko)
 icu.addLocaleData(en)
 use.push(icu)
 
-const NextI18NextInstance = new NextI18Next({
-  defaultLanguage: 'ko',
+const config: InitConfig = {
+  defaultLanguage: 'en',
   defaultNS: 'common',
   fallbackLng: 'en',
   keySeparator: '###',
   localePath: path.resolve('./public/locales'),
   otherLanguages: ['en', 'ko'],
-  browserLanguageDetection: false,
-  serverLanguageDetection: false,
+  browserLanguageDetection: true,
+  serverLanguageDetection: true,
   use,
-})
+}
+
+const NextI18NextInstance = new NextI18Next(config)
 
 export const { appWithTranslation, withTranslation } = NextI18NextInstance
 
